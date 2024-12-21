c87969d9b7f889cd413e514a9af9b0f72e155fcd
Anjali <anjaleesingh14@gmail.com>
2024-12-21 16:27:01 +0530

    + added to be implementation list

--- server-side/src/main.py
+++ server-side/src/main.py
@@ -7,3 +7,11 @@ Base.metadata.create_all(engine)
 
 app.include_router(user_routes.router)
 app.include_router(todo_routes.router)
+
+
+# TO BE IMPLEMENETED:
+'''
+1. Use Mapped and mapped_column instead of Column
+2. Use alembic and make changes to ToDo model: add created_at and updated_at fields
+3. Handle exceptions/errors for all the created routers
+'''
